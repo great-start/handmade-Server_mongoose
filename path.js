61fccc1fe66c9270697e3afa23c864673b769076
ev <zhenya.bill@gmail.com>
2022-04-14 21:18:58 +0300

    Initial

---
+++ path.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+console.log(path.join('one', 'two', 'three'));
+console.log('Склеить участки пути',path.join(__dirname,'one', 'two', 'three'))
+const fullPath = path.resolve('one', 'two', 'three.js');
+console.log('Путь на несколько уровней назад',path.resolve(__dirname,'..','..'))
+console.log('Парсинг', path.parse(fullPath));
+
+console.log('Разделитель в ОС',path.sep);
+console.log('Название файла', path.basename(fullPath));
+console.log('Расширение файла', path.extname(fullPath));
---
+++ process.js
@@ -0,0 +1,9 @@
+const dotenv = require('dotenv').config();
+
+console.log(process.pid);
+
+console.log(process.env.PORT);
+console.log(process.argv);
+console.log(process.exit());
+
+
