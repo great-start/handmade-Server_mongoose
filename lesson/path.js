da926262ef143a6ef1797634b787b01a6d65d829
ev <zhenya.bill@gmail.com>
2022-05-05 16:32:12 +0300

    controllers

--- lesson/path.js
+++ lesson/path.js
@@ -6,6 +6,7 @@ const fullPath = path.resolve('one', 'two', 'three.js');
 console.log('Путь на несколько уровней назад',path.resolve(__dirname,'..','..'))
 console.log('Парсинг', path.parse(fullPath));
 
+
 console.log('Разделитель в ОС',path.sep);
 console.log('Название файла', path.basename(fullPath));
 console.log('Расширение файла', path.extname(fullPath));
